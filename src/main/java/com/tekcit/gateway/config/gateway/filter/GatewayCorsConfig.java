be90f7177bcadc6438b31129a7e1d33b87dec121
rookies-dogun <rookies.dogun@gmail.com>
2025-08-12 13:11:35 +0900

    ãMNMS-328 feat: ãgateway cors 해결

---
+++ src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
@@ -0,0 +1,25 @@
+package com.tekcit.gateway.config.gateway.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:5173"); // React dev 서버 주소
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        config.setAllowCredentials(true); // 쿠키 등 자격증명 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
