c738008d519257568ebd6995a7af3170a47525a8
rookies-dogun <rookies.dogun@gmail.com>
2025-08-20 10:10:05 +0900

    MNMS-346 feat: yaml 값 주석 달기 및 gateway webflux 변경

--- src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
+++ src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
@@ -1,5 +1,6 @@
 package com.tekcit.gateway.config.gateway.filter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,13 +10,18 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @Configuration
 public class GatewayCorsConfig {
 
+
+    @Value("${cors.url:http://localhost:5173}")
+    private String corsUrl;
+
+
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173"); // React dev 서버 주소
-        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
-        config.addAllowedHeader("*"); // 모든 헤더 허용
-        config.setAllowCredentials(true); // 쿠키 등 자격증명 허용
+        config.addAllowedOrigin(corsUrl);
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
