252d34dae7cb6e110792c9734a52e7507e19f082
rookies-dogun <rookies.dogun@gmail.com>
2025-08-26 14:41:39 +0900

    MNMS-504 feat: ãlcors 여려ê개 설정

--- src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
+++ src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
@@ -7,18 +7,23 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class GatewayCorsConfig {
 
 
     @Value("${cors.url:http://localhost:5173}")
-    private String corsUrl;
+    private List<String> corsUrl;
 
 
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin(corsUrl);
+        for(String url : corsUrl){
+
+            config.addAllowedOrigin(url);
+        }
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
