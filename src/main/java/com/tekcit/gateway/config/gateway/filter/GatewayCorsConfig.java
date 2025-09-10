cd1d434b57f4faad2a9899d51ba8b7c93e05d88c
rookies-dogun <rookies.dogun@gmail.com>
2025-09-10 11:15:56 +0900

    ws cors 해결

--- src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
+++ src/main/java/com/tekcit/gateway/config/gateway/filter/GatewayCorsConfig.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 
 import java.util.List;
 
@@ -24,7 +29,6 @@ public class GatewayCorsConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
         for(String url : corsProperties.getUrl()){
-
             config.addAllowedOrigin(url);
         }
         config.addAllowedMethod("*");
@@ -35,5 +39,30 @@ public class GatewayCorsConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+    }
+
+    @Bean
+    public WebFilter webSocketCorsFilter() {
+        return (ServerWebExchange exchange, WebFilterChain chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            String upgradeHeader = request.getHeaders().getFirst(HttpHeaders.UPGRADE);
+
+            if ("websocket".equalsIgnoreCase(upgradeHeader)) {
+                String origin = request.getHeaders().getOrigin();
+                CorsConfiguration corsConfig = new CorsConfiguration();
+
+                if (origin != null && corsProperties.getUrl().contains(origin)) {
+                    corsConfig.addAllowedOrigin(origin);
+                    corsConfig.setAllowCredentials(true);
+                    corsConfig.addAllowedMethod("*");
+                    corsConfig.addAllowedHeader("*");
+                }
+
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", corsConfig);
+            }
+
+            return chain.filter(exchange);
+        };
     }
 }
